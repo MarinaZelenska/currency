6160379a020f033a9ed069327a9cd47fe2f6c83b
Marina Zelenskaya <marina.zelenskaya@motorsport.com>
2021-12-08 14:36:42 +0200

    add parsers

---
+++ app/currency/consts.py
@@ -0,0 +1,13 @@
+CODE_NAME_PRIVATBANK = 'privatbank'
+CODE_NAME_MONOBANK = 'monobank'
+CODE_NAME_VKURSE = 'vkurse.dp.ua'
+CODE_NAME_KREDO = 'KredoBank'
+CODE_NAME_KREDIT_DNEPR = 'CreditDnepr'
+CODE_NAME_AGRICOLE = 'CreditAgricole'
+
+API_PRIVATBANK_URL = "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11"
+API_MONOBANK_URL = "https://api.monobank.ua/bank/currency"
+KREDO_URL = 'https://kredobank.com.ua/info/kursy-valyut/commercial'
+CREDIT_DNEPR_URL = 'https://creditdnepr.com.ua/ru/currency'
+AGRICOLE_URL = 'https://credit-agricole.ua/ru'
+URL_VKURSE_DP = 'http://vkurse.dp.ua/course.json'
